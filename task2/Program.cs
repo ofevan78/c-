e610e5fa16e131993ccd93bbb9f33a6c8c570627
МаксимусПрайм <130079536+ofevan78@users.noreply.github.com>
2023-05-07 17:20:15 +0300

    2,3

---
+++ task2/Program.cs
@@ -0,0 +1,6 @@
+﻿int numberA = new Random().Next(1,100);
+Console.WriteLine(numberA);
+int numberb = new Random().Next(1,100);
+Console.WriteLine(numberb);
+int result = numberA + numberb;
+Console.WriteLine(result);

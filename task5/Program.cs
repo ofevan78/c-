a8b7b38d26a7cf2b43edc83201180056646ef8d9
МаксимусПрайм <130079536+ofevan78@users.noreply.github.com>
2023-05-29 18:58:51 +0300

    5 задача масcив от максимального до мин

--- task5/Program.cs
+++ task5/Program.cs
@@ -1,4 +1,4 @@
-﻿int[] arr = { 7, 8, 3, 6, 4, 1, 2, 3, 3,8,11,35,89,75 };
+﻿int[] arr = { 7, 8, 3, 6, 4, 1, 2, 3, 3, 8, 11, 35, 89, 75 };
 void PrintArry(int[] array)
 {
     int count = array.Length;
@@ -11,19 +11,21 @@ void PrintArry(int[] array)
 
 void SelectionSort(int[] array)
 {
-    for (int i = 0; i < array.Length-1; i++)
+    for (int i = 0; i < array.Length; i++)
     {
-        int minPosition = i;
-        for (int j = i + 1; j < array.Length; j++)
+        int maxPosition = i;
+        for (int j = i + 1; j < array.Length; j++) 
         {
-            if (array[j] < array[minPosition]) minPosition = j;
+            if (array[j] > array[maxPosition]) maxPosition = j;
         }
 
 
 
+
+
         int temporary = array[i];
-        array[i] = array[minPosition];
-        array[minPosition] = temporary;
+        array[i] = array[maxPosition];
+        array[maxPosition] = temporary;
 
 
 
